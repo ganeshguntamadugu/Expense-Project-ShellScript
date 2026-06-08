3f75f7b576311fcaeab5de43d4d910b39eea96a2
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-06-08 20:54:31 +0530

    Updated something in Expense Project

--- backend.sh
+++ backend.sh
@@ -36,7 +36,7 @@ VALIDATE(){
         echo -e "$Y Enabled$N $2 version 20" | tee -a $Log_file
 
         dnf install $2 -y &>>$Log_file
-        echo -e "$2 version 20 installed $G Successful$N" | tee -a $Log_file
+        echo -e "$2 version 20 installed$G Successful$N" | tee -a $Log_file
         
     else
         echo -e "$2 is$Y Already$N installed, nothing to do" | tee -a $Log_file
@@ -64,14 +64,14 @@ id expense &>>$Log_file
 if [ $? -ne 0 ]
 then
     useradd expense
-    echo -e "Created a user $G 'Expense'$N" | tee -a $Log_file
+    echo -e "Created a user$G 'Expense'$N" | tee -a $Log_file
 else
-    echo -e "User Expense is $Y Already$N Exists" | tee -a $Log_file
+    echo -e "User Expense is$Y Already$N Exists" | tee -a $Log_file
 fi
 
 echo "" | tee -a $Log_file
 curl -o /tmp/backend.tar.gz https://raw.githubusercontent.com/daws-90s/expense-documentation/refs/heads/main/artifacts/expense-backend-v3.tar.gz &>>$Log_file
-echo -e "Application is $G Downloaded$N" | tee -a $Log_file
+echo -e "Application is$G Downloaded$N" | tee -a $Log_file
 
 cd /app
 tar -xzf /tmp/backend.tar.gz 
@@ -79,44 +79,44 @@ tar -xzf /tmp/backend.tar.gz
 echo "" | tee -a $Log_file
 cd /app
 npm install &>>$Log_file
-echo -e "Dependencies are $G Installed$N" | tee -a $Log_file
+echo -e "Dependencies are$G Installed$N" | tee -a $Log_file
 
 echo "" | tee -a $Log_file
 cp /home/ec2-user/Expense-Project-ShellScript/backend-config /etc/systemd/system/backend.service
-echo -e "Backend configurations are $G Copied$N" | tee -a $Log_file
+echo -e "Backend configurations are$G Copied$N" | tee -a $Log_file
 
 echo "" | tee -a $Log_file
 dnf list installed mysql &>>$Log_file
 if [ $? -ne 0 ]
 then
     dnf install mysql -y &>>$Log_file
-    echo -e "MySQL installation $G Successfull$N" | tee -a $Log_file
+    echo -e "MySQL installation$G Successfull$N" | tee -a $Log_file
 else
-    echo -e "MySQL is $Y Already$N installed" | tee -a $Log_file
+    echo -e "MySQL is$Y Already$N installed" | tee -a $Log_file
 fi
 
 echo "" | tee -a $Log_file
 mysql -h mysql.gangs.shop -u root -pExpenseApp@1 < /app/schema/backend.sql &>>$Log_file
 if [ $? -ne 0 ]
 then
-    echo -e "Schema integration $R Failed$N, Please check the error" | tee -a $Log_file
+    echo -e "Schema integration$R Failed$N, Please check the error" | tee -a $Log_file
     exit 1
 else
-    echo -e "Schema integration $G Successfull$N" | tee -a $Log_file
+    echo -e "Schema integration$G Successfull$N" | tee -a $Log_file
 fi
 
 
 echo "" | tee -a $Log_file
 systemctl daemon-reload
-echo -e "Daemon reload $G Successfull$N" | tee -a $Log_file
+echo -e "Daemon reload$G Successfull$N" | tee -a $Log_file
 
 echo "" | tee -a $Log_file
 systemctl enable backend
-echo -e "Enabled backend $G Successfull$N" | tee -a $Log_file
+echo -e "Enabled backend$G Successfull$N" | tee -a $Log_file
 
 echo "" | tee -a $Log_file
 systemctl restart backend
-echo -e "Restarted backend $G Successfull$N" | tee -a $Log_file
+echo -e "Restarted backend$G Successfull$N" | tee -a $Log_file
 
 echo "" | tee -a $Log_file
 echo -e "Script$G Completed$N executing at $(date)" | tee -a $Log_file 
